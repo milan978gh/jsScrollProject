ea3bab42e7b4bc636502a3d97d0e1bfd8dd540d1
milan978gh <milan978@gmail.com>
2020-11-04 20:17:06 +0100

    update

--- js/main.js
+++ js/main.js
@@ -3,11 +3,57 @@
 // slice extracts a section of a string without modifying original string
 //offsetTop - A Number, representing the top position of the element, in pixels
 
+
+
+const date = document.getElementById('date');
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
 // ********** set date ************
+date.innerHTML = new Date().getFullYear();
+
 
 // ********** close links ************
+navToggle.addEventListener('click', function () {
+    // linksContainer.classList.toggle('show-links');
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    } else {
+        linksContainer.style.height = 0;
+    }
+});
+
 
 // ********** fixed navbar ************
+window.addEventListener('scroll', function () {
+    // moja ideja
+    /*if (window.pageYOffset > 500) {
+         navbar.classList.add('fixed-nav');
+         topLink.classList.add('show-link');
+     } else {
+         navbar.classList.remove('fixed-nav');
+         topLink.classList.remove('show-link');
+     }*/
 
+    // njegova ideja
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav');
+    } else {
+        navbar.classList.remove('fixed-nav');
+    }
+    if (scrollHeight > 500) {
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove('show-link');
+    }
+});
 // ********** smooth scroll ************
 // select links
